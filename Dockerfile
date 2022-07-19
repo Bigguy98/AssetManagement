a48123525331fb41d058894c6ff4b4ccee2fbe78
bugsmaker <vuthanhtunghayquen98@gmail.com>
2022-07-19 11:53:27 +0700

    update

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM openjdk:11
-EXPOSE 8080
 ADD /target/asset-management-0.0.1-SNAPSHOT.jar app.jar
-ENTRYPOINT ["java","-jar","app.jar"]
+COPY run.sh /run.sh
+EXPOSE 8080
+ENTRYPOINT ["/bin/sh","-c","/run.sh"]
---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+ 
+if [ ! -z $SEEKER_SERVER_URL ];
+then
+curl -k -o /tmp/seeker.zip "$SEEKER_SERVER_URL/rest/api/latest/installers/agents/binaries/JAVA"
+unzip /tmp/seeker.zip -d /tmp/seeker
+JAVA_OPTS="$JAVA_OPTS -javaagent:/tmp/seeker/seeker-agent.jar -Dseeker.server.url=$SEEKER_SERVER_URL"
+fi
+     
+java $JAVA_OPTS -jar /app.jar --server.address=0.0.0.0
